9ebb54bd8b9b78a4fd67596b39f8be062df8872e
zeann3th <longhienly112@gmail.com>
2024-12-08 08:59:01 +0700

    [UPDATE]: Update NextConfig to allow certain origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} **/
-
-module.exports = {
+const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
@@ -21,4 +20,29 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/auth/callback",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_API_URL,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "HEAD,GET,POST,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
